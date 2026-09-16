b30d71ff5c7511c5515bdae6ab42aed36de4dcaf
Walid Mouhoub <6309365@cegepmontpetit.ca>
2026-09-16 10:56:38 -0400

    FCT Zombie Edit
    Ajout de la View Edit et des actions dans ZombieController

--- ZombieParty/ZombieParty/Controllers/ZombieController.cs
+++ ZombieParty/ZombieParty/Controllers/ZombieController.cs
@@ -41,6 +41,19 @@ namespace ZombieParty.Controllers
             return View(zombieVM);
         }
 
+        public IActionResult Edit(int id)
+        {
+            ZombieVM zombieVM = new ZombieVM();
+            zombieVM.Zombie = _baseDonnees.Zombies.Find(id);
+            zombieVM.ZombieTypeSelectList = _baseDonnees.ZombieTypes.Select(t => new SelectListItem
+            {
+                Text = t.TypeName,
+                Value = t.Id.ToString()
+            }).OrderBy(t => t.Text);
+
+            return View(zombieVM);
+        }
+
         [HttpPost]
         public IActionResult Create(ZombieVM zombieVM)
         {
@@ -50,6 +63,28 @@ namespace ZombieParty.Controllers
                 _baseDonnees.Zombies.Add(zombieVM.Zombie);
                 _baseDonnees.SaveChanges();
                 TempData["Success"] = $"Zombie {zombieVM.Zombie.Name} added";
+                return this.RedirectToAction("Index");
+            }
+            zombieVM.ZombieTypeSelectList = _baseDonnees.ZombieTypes.Select(t => new SelectListItem
+            {
+                Text = t.TypeName,
+                Value = t.Id.ToString()
+            }).OrderBy(t => t.Text);
+
+            return View(zombieVM);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ZombieVM zombieVM)
+        {
+            //Si le modèle est valide le zombie est ajouté et nous sommes redirigé vers index.
+            if (ModelState.IsValid)
+            {
+                _baseDonnees.Zombies.Update(zombieVM.Zombie);
+                _baseDonnees.SaveChanges();
+                TempData["Success"] = $"Zombie {zombieVM.Zombie.Name} has been modified";
                 return this.RedirectToAction("Index");
             }
             zombieVM.ZombieTypeSelectList = _baseDonnees.ZombieTypes.Select(t => new SelectListItem
