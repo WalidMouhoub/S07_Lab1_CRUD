3086980b467775f1f67e6687319a5afdbd311489
Walid Mouhoub <6309365@cegepmontpetit.ca>
2026-09-16 10:03:34 -0400

    FCT Seed Zombie ZombieType et Weapon
    Création données de départ pour Zombie, ZombieType et Weapon

--- ZombieParty/ZombieParty/Models/Data/ModelBuilderDataGenerator.cs
+++ ZombieParty/ZombieParty/Models/Data/ModelBuilderDataGenerator.cs
@@ -48,7 +48,8 @@ namespace ZombieParty.Data
             #region Weapon
             builder.Entity<Weapon>().HasData(
                 new Weapon() { WeaponId = 1, Name = "Leopold", Description = "This is to cut your friendly zombies", Force = 200, Image = "https://i.ibb.co/y5nCnXT/dagger-gfce31e967-1920.png", Price = 421, Qty = 21, QtyBought = 3, CreatedDate = new DateTime(1999, 01, 1) },
-                new Weapon() { WeaponId = 2, Name = "Master Shield", Description = "An awesome Stainless Steel Shield", Force = 10, Image = "https://publicdomainvectors.org/photos/Shield-ClassicMedieval1.png", Price = 500, Qty = 30, QtyBought = 1, CreatedDate = new DateTime(1999, 01, 1) }
+                new Weapon() { WeaponId = 2, Name = "Master Shield", Description = "An awesome Stainless Steel Shield", Force = 10, Image = "https://publicdomainvectors.org/photos/Shield-ClassicMedieval1.png", Price = 500, Qty = 30, QtyBought = 1, CreatedDate = new DateTime(1999, 01, 1) },
+                new Weapon() { WeaponId = 3, Name = "Primoridum Vandal", Description = "The best gun in the game", Force = 160, Image = "https://valorantstrike.com/valorant-primordium-skins/", Price = 2175, Qty = 20, QtyBought = 10, CreatedDate = new DateTime(2024, 08, 1) }
             );
             #endregion
         }
